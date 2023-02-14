1eac1c76e5f7899241aaf1f8c863f7b38039440b
KEDARNATHCHAVAN <kedarnathpc2002@gmail.com>
2023-02-14 22:26:19 +0530

    updated

---
+++ containers/pair.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+    pair<int,string> p;
+    pair<int,string> pp(2,"chavan");
+    pair<int,int> ppp;
+    ppp = make_pair(3,5);
+    p.first = 1;
+    p.second = "kedar";
+    cout << p.first << " " << p.second;
+    return 0;
+}
--- containers/unordered_map.cpp
+++ containers/unordered_map.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main (){
+int main ()
+{
     unordered_map<int, string> m;
     m[3] = "kedar";
     m[3] = "nath";
@@ -9,11 +10,12 @@ int main (){
     m[8] = "walchand";
     m[4] = "college";
     m.insert(make_pair(2,"hello"));
-    for(auto i : m){
+    for(auto i : m)
+    {
         cout << i.first << " " << i.second << endl;
     }
 
-    //begin, end, count, find, empty, erase
-    
+    // begin, end, count, find, empty, erase
+
     return 0;
 }
